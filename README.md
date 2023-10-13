b2a276a95176abe2c79ee13705b15d3b3d27bc87
Simone Green <simoneg80@yahoo.com>
2023-10-13 00:51:26 -0400

    "Final MVP"

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Project-1
+# Simone Green SEI-Project 1-Rubber Man
+
+## Game Description 
+
+Try this classic game of Hangman with a Twist! Inspired by One Piece, Rubber Man challenges users to test how well they know Luffy. With 5 tries, guess the word and see who or what is uncovered. Be sure to guess wisely! You don't want to make Luffy angry.
+
+The main goal of the game is to guess the correct word. If the word is guessed before the player runs out of tries, they win! Otherwise, they will see images of 'Angry Luffy' with each incorrect guess. It is simple and fun!
+
+## Game Images 
+
+
+## Technologies
+    HTML    
+    CSS 
+    JavaScript 
+
+## Challenging Code
+
+
+
+## Game Link
+
+
+
+## Next Steps
+There are a number of updates i'd like to include:
+    A gif or video looping in the background to add movement to the page
+    Images of other characters when corresponding words are guessed 
+    Additional rounds of the game with categories to choose from
+    Functionality to reset the game 
+
+## The MIT License (MIT)
+Copyright (c) 2023 Simone Green
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const keyPadArr = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "h", "i", "j", "k", "l",
     "m", "n", "o", "p", "q", "r",
     "s", "t", "u", "v", "w", "x",
-    "y", "z"
+    "y", "z",
 ]
 //Array to hold answers for Name category  
 const nameArr = [
@@ -11,22 +11,29 @@ const nameArr = [
     "ace",
     "garp",
     "dragon",
-    "sabo"
+    "sabo",
+    "zoro",
+    "usopp",
+    "robin",
+    "nami",
+    "brook",
+    "franky",
+    "sanji",
+    "chopper"   
 ]
-
-//Select play button from CSS and remove when clicked 
-//Display text lines and keypad
+//Declare elements from css
 const playGame = document.querySelector(".play-button");
 const startText = document.querySelector(".start-game");
 const textDisplay = document.querySelector(".text-line");
 const keyPad = document.querySelector(".keypad");
 const keyPadButtons = document.querySelectorAll(".buttonEL")
 const angryLuffyContainer = document.querySelector(".Angry-Luffy-Container")
+const gameBoard = document.querySelector(".Game")
+const messageBoard = document.querySelector(".messageBoard")
 
 // declare varible to store random word 
 let selectedWrd;
 
-//1.
 //Start game by clicking pplay button and removing it from display 
 //Then display keyPad
 playGame.addEventListener("click", function (evt) {
@@ -36,144 +43,85 @@ playGame.addEventListener("click", function (evt) {
     keyPad.style.display = "inline-block";
   });
   
-  //2.
   //Select a random name from nameArr 
   let secretWrd = function(name) {
       return nameArr[(Math.floor(Math.random() * nameArr.length))].split("");
   }
-  selectedWrd = secretWrd();
-  
+selectedWrd = secretWrd();
+
+//Iterate over secret word to isolate each letter and add styling  
 selectedWrd.forEach((letter, idx) => {
     let letterEL = document.createElement("div");
     letterEL.classList.add("selected")
     letterEL.setAttribute("id", idx)
-    // letterEL.innerHTML = letter;
-    // console.log(letter + "This is the letter")
     textDisplay.appendChild(letterEL) 
-    // letterEL.style.display = "inline"
     letterEL.style.borderBottom = "2px black solid"
     letterEL.style.margin = "5px";
     letterEL.style.width = "10px";
-    // letterEL.style.opacity = "0"
 
-     ///
-
+     //Add event listener to for when a key on the key pad is clicked 
     keyPadButtons.forEach((button)=> {
     button.addEventListener("click", function (evt ){
-        if(keyPadButtons.button === evt.innerText) {
+        if(keyPadButtons.button === evt.innerText && gameOver === false) {
             button.style.opacity = "20%"    
-    }    
+        }    
     }) 
   });
 })
 
-
-
-//3. 
-//Create function to compare selcted letters to selected word 
+//Declare game's current status 
+//Declare how many letters are remaining 
+let gameOver = false;
 let lettersRemaining = selectedWrd.length
 let turnsRemaining = 5;
-const compareLetters = function(evt) {  
+
+//Create function to compare selcted letters to selected word 
+const compareLetters = function(evt) { 
+    if(gameOver === true) return;
     //Get Dom elements from keyPad
     const selectedLetter = evt.target.innerText;
 
-// if the word includes a letter selected(the selectedLetter var) from keyPad
-//iterate over the selectedWrd 
-
+    //If the word includes a letter selected(the selectedLetter var), from keyPad
+    //iterate over the selectedWrd 
     if(selectedWrd.includes(selectedLetter)) {
         selectedWrd.forEach((letter, idx) => {
             let correctLetter = document.getElementById(idx);
             if(letter.toLowerCase() == selectedLetter.toLowerCase()) {
                 correctLetter.innerHTML = selectedLetter;
-            }
-            
+            }  
         }) 
-        console.log(lettersRemaining)
-        lettersRemaining--
-        console.log(lettersRemaining)
-        if (lettersRemaining === 0) {
-            alert("Player Wins!")
-        }
-    } else  {
+        //Reduce letter remaining
+        //If letters remaing qual zero the word is complete and player wins
         
+        lettersRemaining--
+        if (lettersRemaining === 0) {
+            messageBoard.innerHTML = "Player Wins!"
+            gameOver = true;
+        }
+    } else  { //Else if turn remaining equal zero player loses
+        //Set "Angry Luffy" photo to appear for wrong guesses 
         const angryLuffy = document.createElement("img")
         angryLuffy.src = "./Angry-Luffy.png";
         angryLuffyContainer.appendChild(angryLuffy) 
         turnsRemaining--
         if (turnsRemaining === 0) {
-            //message.innerText="" //with creating a div 
-            alert("Player Loses! :( ")
-        }
+            messageBoard.innerHTML = "Player Loses!"
+            gameOver = true;   
+        }  
     }
-    
+    //if game ends create a play again button and reload the page
+    if(gameOver) {
+        console.log("game over")
+        const playAgainBtn = document.createElement("button")
+        playAgainBtn.textContent = "Play Again"
+        gameBoard.appendChild(playAgainBtn)
+        playAgainBtn.addEventListener("click", function () {
+            window.location.reload()
+        })
+    } 
 }
-
+//when key is clicked compare/match letter in array with key inner text 
 keyPadButtons.forEach((button)=> {
-    button.addEventListener("click", compareLetters)
-      
+    button.addEventListener("click", compareLetters) 
   });
 
-
-
-
-
-
-
-
-///
-//  keyPadButtons.forEach((button)=> {
-//     button.addEventListener("click", function (evt ){
-//         if(keyPadButtons.button === evt.innerText) {
-//             button.style.opacity = "20%"
-//             // selectedWrd.innerText.style.opacity = "100%"
-
-
-
-//         }     
-//     }) 
-//   });
-
-///
-
-
-
-
-
-
-
-//     keyPadLetters.forEach(letter => {
-//         if(keyPadArr.includes(letter)) {
-//             clickedLetter.push(letter)
-//         } else {
-//             console.log(`letter ${letter} is not in the array`)
-//         }
-//     })
-//     console.log(clickedLetter);
-// }
-
-
-// keyPadButton.addEventListener("click", compareLetters);
-// console.log(keyPadButton);
-
-
-
-
-
-//Asign secretWrd to keypad selection
-//If true display letter and add a border line under letter 
-
-// const letterChoice = keyPadArr.forEach(letter => { 
-//     console.log(letter);   
-//     });
-
-
-// keyPad.addEventListener("click", function (evt) {
-//     keyPadArr.forEach(letter => { 
-//         if (keyPadArr[letter] === secretWrd) {
-//             // keyPadArr.style = border
-//             console.log(keyPadArr[letter], "This letter is clicked")
-//           } 
-
-         
-//         });
-// });
